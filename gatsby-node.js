bce9c3abd4c68d94250dbd785c96687b62fd5b17
Jeff Pottruff <jeffpottruff@gmail.com>
2020-07-09 13:04:03 -0400

    FIX resolve blog post path in gatsy-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,45 @@
  */
 
 // You can delete this file if you're not using it
+
+// NOTE: path sis a standard node.js module
+const path = require('path');
+
+// NOTE: might be some redux stuff going on here with the ActioCreators;
+// Basically trying to registering posts so we can use the urls (eg post-one)
+exports.createPages = ({boundActionCreators, graphql}) => {
+    // Destructing - pull out createPage
+    const { createPage } = boundActionCreators;
+
+    const postTemaplate = path.resolve('src/templates/blog-post.js');
+
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                node{
+                    html
+                    id
+                    frontmatter {
+                    path
+                    title
+                    date
+                    author
+                    }
+                }
+                }
+            }
+        }
+    `).then(res => {
+        if (res.errors) {
+            return Promise.reject(res.errors);
+        }
+
+        res.data.allMarkdownRemark.edges.forEach(({node}) => {
+            createPage({
+                path: node.frontmatter.path,
+                component: postTemaplate,
+            })
+        })
+    })
+}
